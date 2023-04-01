2b014de7b5967eaaa5d6f4137a878d1dd00373b3
RLee <ysryanlee@gmail.com>
2023-04-01 17:40:14 -0400

    API Testing

---
+++ Personal Projects/API testing/Study Notes.md
@@ -0,0 +1,10 @@
+***API STUDIES***
+
+**Terminology**
+1. POST - create new record
+2. GET - read
+3. PUT - if record exists, update
+4. PATCH - update/mod
+5. DELETE - delete
+
+When getting data via REST API, they can have libraries and functions already created for you to use. (e.g Kaggle)
---
+++ Personal Projects/API testing/kaggle_happiness_data_api.py
@@ -0,0 +1,18 @@
+import kaggle
+from kaggle.api.kaggle_api_extended import KaggleApi
+import os
+
+#checks if this folder exists
+path= r'C:\Users\RIDGE\.kaggle'
+isExist = os.path.exists(path)
+if not isExist:
+    os.makedirs(path)
+    print(".kaggle created in C:\x5cUsers\x5cRIDGE\x5c")
+    
+
+#access the API
+api = KaggleApi()
+api.authenticate()
+
+#download dataset
+api.dataset_download_file('world-happiness-report-2023', 'WHR2023.csv')
